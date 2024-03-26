7b3ea67a1d229d53fe5972f5f7029f9a068a0184
ksalarang <anbleidd@gmail.com>
2024-03-26 16:12:49 +0500

    add level data scriptable objects

---
+++ Assets/Scripts/PlayScene/Data/Levels/LevelBundleData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace PlayScene.Data.Levels
+{
+    [CreateAssetMenu(fileName = "New LevelBundleData", menuName = "Level Bundle Data", order = 0)]
+    public class LevelBundleData : ScriptableObject
+    {
+        [SerializeField]
+        private LevelData[] _levelDataList;
+
+        public LevelData[] LevelDataList => _levelDataList;
+    }
+}
---
+++ Assets/Scripts/PlayScene/Data/Levels/LevelData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlayScene.Data.Levels
+{
+    [Serializable]
+    public class LevelData
+    {
+        [SerializeField]
+        private int _cellAmount;
+
+        [SerializeField]
+        private CardBundleData _cardBundleData;
+    }
+}
