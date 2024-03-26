46418443062fac68a8d734cc154a37472e6ce865
ksalarang <anbleidd@gmail.com>
2024-03-26 14:00:35 +0500

    setup zenject binding installer

---
+++ Assets/Scripts/ZenjectBindings/BindingInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Zenject;
+
+namespace ZenjectBindings
+{
+    public class BindingInstaller : MonoInstaller
+    {
+        
+
+        public override void InstallBindings() {
+            
+        }
+    
+        void bind<T>(T instance) {
+            Container.BindInstance(instance);
+        }
+    }
+}
